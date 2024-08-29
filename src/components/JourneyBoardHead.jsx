e4de1b38ddf88e19d09e56a9d06aa5629214f970
kirti <kirti.20cs@saividya.ac.in>
2024-08-29 19:52:02 +0530

    make JourneyBoardHead responsive

--- src/components/JourneyBoardHead.jsx
+++ src/components/JourneyBoardHead.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function JourneyBoardHead() {
     return (
-        <div className='font-poppins mb-10'>
+        <div className='font-poppins  mb-10'>
             <div className='flex justify-between '>
-                <h1 className='text-[#0029FF] font-bold text-2xl mt-6 mb-8'>Technical Project Management</h1>
-                <button className='bg-[#0029FF] text-white rounded-2xl px-4 py-2 self-center text-sm'>Submit task</button>
+                <h1 className='text-[#0029FF] font-bold text-lg sm:text-2xl mt-12 sm:mt-6 mb-8'>Technical Project Management</h1>
+                <button className='bg-[#0029FF] text-white rounded sm:rounded-2xl p-1 sm:px-4 sm:py-2 self-center text-xs sm:text-sm'>Submit task</button>
             </div>
             <div className='bg-[#E9ECEF] rounded-md p-5'>
                 <h3 className='font-semibold text-xl pb-3'>Explore the world of management</h3>
