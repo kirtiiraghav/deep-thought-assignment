efb3cdf624c51c40a0b9d5a25bc58ac8e726081e
kirti <kirti.20cs@saividya.ac.in>
2024-08-29 19:52:49 +0530

    make Header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ import userImg from '../../assests/images/userImg.svg'
 export default function Header() {
     return (
         <header className='max-w-screen-xl mx-auto bg-[#F0F0F0] shadow-md '>
-            <div className='px-10 py-4 flex justify-between items-center'>
+            <div className='px-2 py-2 sm:px-10 sm:py-4 flex justify-between items-center'>
                 <div className='cursor-pointer'><a target='_blank' href="https://deepthought.education/">
-                    <img src={logo} alt="deep thought logo" />
+                    <img className='w-[80%] sm:w-full' src={logo} alt="deep thought logo" />
                 </a>
                 </div>
                 <div>
-                    <ul className='flex gap-4 items-center'>
+                    <ul className='flex gap-1 sm:gap-4 items-center'>
                         <li className='cursor-pointer'><img src={home} alt="home" /></li>
                         <li className='cursor-pointer'><img src={tool} alt="tool" /></li>
                         <li className='cursor-pointer relative'><img src={notification} alt="tool" /><span className='bg-[#FF2C55] h-2 w-2 rounded-[50%] absolute top-2 right-2'></span></li>
