698a5e6815c0c7a05d142adb32e9d36c02099f57
kirti <kirti.20cs@saividya.ac.in>
2024-08-30 09:25:11 +0530

    remove hard coded url from YouTubeEmbed

--- src/components/JourneyCard.jsx
+++ src/components/JourneyCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const JourneyCard = ({ title, description, children, icon }) => {
-    console.log(children);
     return (
         <div className="font-poppins w-full md:w-[450px] rounded-xl overflow-hidden shadow-md mb-6">
             <div className="bg-[#000] text-white py-2 px-3  flex justify-between">
--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { projectData } from '../../projectData';
 
 const YouTubeEmbed = () => {
-    // Hard-coded video ID for the YouTube video
-    const videoId = "TiMRwri1xJ8";
-    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    const data = projectData
+    // console.log(data.tasks[0].assets[0].asset_content);
     const title = "YouTube Video";
 
     return (
         <div className="youtube-embed-container" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', background: '#000' }}>
             <iframe
-                src={embedUrl}
+                src={data.tasks[0].assets[0].asset_content}
                 title={title}
                 style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                 frameBorder="0"
